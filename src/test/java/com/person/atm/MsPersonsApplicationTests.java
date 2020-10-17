32832333339e2d93d16f0ce3e8e2f4510434d5ee
Cesar Guerrero <ce_david14@outlook.com.pe>
2020-10-16 21:31:07 -0500

    first commit

---
+++ src/test/java/com/person/atm/MsPersonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
